2a4cd22878aad4287d61c06a6ec1d6485c5fd0c8
hydok <hydok@naver.com>
2022-11-09 14:12:44 +0900

    쿼리 작성 및 테스트 통신

--- app/src/main/java/io/hydok/apolloclient/MainActivity.kt
+++ app/src/main/java/io/hydok/apolloclient/MainActivity.kt
@@ -2,10 +2,31 @@ package io.hydok.apolloclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo3.ApolloClient
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql/")
+            .build()
+
+
+
+        lifecycleScope.launch {
+            val gqlResult = apolloClient.query(GetCharactersQuery()).execute()
+
+            Log.d("GraphQL", gqlResult.operation.document())
+            Log.d("GraphQL", gqlResult.operation.id())
+
+            gqlResult.data?.characters?.results?.forEach {
+                Log.d("GraphQL", it.toString())
+            }
+        }
     }
 }
